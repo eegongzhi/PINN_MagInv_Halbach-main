4de2f27f6a051886aeed4a2fd5950fcf1a20e4c8
nexusgz <nexusgz@hotmail.com>
2025-09-06 20:42:04 +0200

    Update magnet_discretization.m

--- pole_10/data_generation/magnet_discretization.m
+++ pole_10/data_generation/magnet_discretization.m
@@ -91,40 +91,6 @@ for j = 1:size(theta_derivative_grid, 2)
 end
 
 
-% %% generate grid for z-direction derivatives
-% z_derivative_grid = cell(num_grid_x, num_grid_y, num_grid_z+1);
-% M_z_grid = z_derivative_grid;
-% for k = 1:size(z_derivative_grid, 3)
-%     for j = 1:size(z_derivative_grid, 2)
-%         for i = 1:size(z_derivative_grid, 1)
-%             % loop every point z_derivative_grid{i,j,k}
-%             z_derivative_grid{i,j,k} = [hx/2+(i-1)*hx,...
-%                 hy/2+(j-1)*hy,...
-%                 (k-1)*hz];
-%             % calculate magnitude
-%             valuez = M*exp(-((hx/2+(i-1)*hx-x_0)^2/(2*sigma_x^2)+...
-%                 (hy/2+(j-1)*hy-y_0)^2/(2*sigma_y^2)));
-%             valuey = exp(-((hx/2+(i-1)*hx-x_0)^2/(2*sigma_x^2)+...
-%                 ((k-1)*hz-z_0)^2/(2*sigma_z^2)));
-%             valuex = exp(-(((k-1)*hz-z_0)^2/(2*sigma_z^2)+...
-%                 (hy/2+(j-1)*hy-y_0)^2/(2*sigma_y^2)));
-% %             mag = valuez *valuey *valuex;
-%             mag = 1;
-%             dis_xy = sqrt(((i-1)*hx-x_0)^2 + (hy/2+(j-1)*hy-y_0)^2);
-% %             mag = M * (1- dis_xy/(2*d_max));
-% 
-%             % calculate the theta
-%             theta = theta_max * (dis_xy/d_max);
-%             % recover x-y-z
-%             x_comp = mag*sin(theta)*(hx/2+(i-1)*hx-x_0)/dis_xy;
-%             y_comp = mag*sin(theta)*(hy/2+(j-1)*hy-y_0)/dis_xy;
-%             z_comp = mag*cos(theta);
-% 
-%             M_z_grid{i,j,k} = [x_comp,y_comp,z_comp];   % The M vector @ the grid for M_z
-%         end
-%     end
-% end
-
 %% generate div(M) on the center grid
 center_grid = cell(num_grid_rho,num_grid_theta);
 M_center = center_grid;
@@ -159,49 +125,4 @@ end
 clear divM dMrdr dMtdt i j
 save magnet_discretization
 
-%% visualization: plot M_x_grid on x_derivative_grid
-% % generate meshgrid from "x_derivative_grid"
-% x_coord = zeros(1, size(x_derivative_grid, 1));
-% y_coord = zeros(1, size(x_derivative_grid, 2));
-% z_coord = zeros(1, size(x_derivative_grid, 3));
-% for idx = 1:length(x_coord)
-%     x_coord(idx) = x_derivative_grid{idx,1,1}(1);
-% end
-% for idy = 1:length(y_coord) 
-%     y_coord(idy) = x_derivative_grid{1,idy,1}(2);
-% end
-% for idz = 1:length(z_coord) 
-%     z_coord(idz) = x_derivative_grid{1,1,idz}(3);
-% end
-% [X,Y,Z] = meshgrid(x_coord, y_coord, z_coord);
-% Mx_3D = zeros(size(M_x_grid));
-% My_3D = zeros(size(M_x_grid));
-% Mz_3D = zeros(size(M_x_grid));
-% 
-% for idx = 1:size(Mx_3D,1)
-%     for idy = 1:size(Mx_3D,2)
-%         for idz = 1:size(Mx_3D,3)
-%             Mx_3D(idx,idy,idz) = M_x_grid{idx,idy,idz}(1);
-%         end
-%     end
-% end
-% for idx = 1:size(My_3D,1)
-%     for idy = 1:size(My_3D,2)
-%         for idz = 1:size(My_3D,3)
-%             My_3D(idx,idy,idz) = M_x_grid{idx,idy,idz}(2);
-%         end
-%     end
-% end
-% for idx = 1:size(Mz_3D,1)
-%     for idy = 1:size(Mz_3D,2)
-%         for idz = 1:size(Mz_3D,3)
-%             Mz_3D(idx,idy,idz) = M_x_grid{idx,idy,idz}(3);
-%         end
-%     end
-% end
-% figure
-% quiver3(X,Y,Z,permute(Mx_3D,[2,1,3]),permute(My_3D,[2,1,3]),permute(Mz_3D,[2,1,3]))
-% xlabel('x')
-% ylabel('y')
-% zlabel('z')
-% 
+
